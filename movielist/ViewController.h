3a405389ef8c00f4b84712566b50e999bf85c19f
omer cohen <omer260792@gmail.com>
2018-11-11 10:57:15 +0200

    Initial Commit

---
+++ movielist/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  movielist
+//
+//  Created by Omer Cohen on 11/11/18.
+//  Copyright © 2018 Omer Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ movielist/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  movielist
+//
+//  Created by Omer Cohen on 11/11/18.
+//  Copyright © 2018 Omer Cohen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
